7c76ab2c98e58eb2b2c8b68c1bf759e9ea707408
Andrzej Szygula <andrzej.szygula@outlook.com>
2019-04-12 13:25:16 +0200

    first  commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Program;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A x = new B();
+            x.VirtualFun();
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
